735936f89f8b30e6124c4d4e09a01ab7a1562668
austincrisp <austi@AUSTINBLADE>
2017-01-24 14:37:25 -0500

    Tables created; Normal mode select statements complete

---
+++ Movie Database/normal_mode_selects.sql
@@ -0,0 +1,17 @@
+﻿SELECT Person.age, Movie.title, rating
+FROM Review
+INNER JOIN Movie ON Movie.id = Review.movie
+INNER JOIN Person ON Person.id = Review.person
+WHERE Movie.title = 'Toy Story';
+
+SELECT Person.age, Person.occupation
+From Person
+WHERE Person.age < 40;
+
+SELECT * FROM Movie
+WHERE imdb_url IS NULL;
+
+SELECT Movie.title, rating
+FROM Review
+INNER JOIN Movie ON Movie.id = Review.movie
+WHERE rating <= 2;
